4b0a5d28d9103d537e937ad17e7619b6e3836d8b
Alec Brownlie <alecbrownlie@gmail.com>
2019-02-16 19:32:52 -0800

    task 3 sort of implemented

---
+++ src/com/algorithms/SieveOfEratosthenes.java
@@ -0,0 +1,56 @@
+package com.algorithms;
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+
+public class SieveOfEratosthenes {
+
+	public static Integer computeGCD(Integer m, Integer n) {
+		Integer result = 1;
+		List<Integer> primeFactors = getPrimeFactors(m);
+		primeFactors.retainAll(getPrimeFactors(n));
+		for (Integer i : primeFactors) {
+			result *= i;
+		}
+		return result;
+	}
+
+	private static List<Integer> getPrimeFactors(Integer number){
+		int copy = number;
+		List<Integer> factors = new ArrayList();
+		for (int i = 2; i <= copy; i++) {
+			if (copy % i == 0 && getPrimeNumbers(number).contains(i)) {
+				factors.add(i);
+				copy /= i;
+				i--;
+			}
+		}
+		return factors;
+	}
+
+	// from Section (1.1)
+	private static List<Integer> getPrimeNumbers(Integer n) {
+		List<Integer> L = new ArrayList<>();
+		boolean A[] = new boolean[n]; 
+
+        for (int p = 2; p < n; p++) {
+        	A[p] = true;
+      	}
+
+      	for (int p = 2; p < Math.sqrt(n); p++) {
+        	if (A[p] == true) {
+            	for(int j = (p * p); j < n; j = j + p) {
+               		A[j] = false;
+            	}
+        	}
+      	}
+
+      	for (int p = 2; p < n; p++) {
+      		if (A[p] == true) {
+      			L.add(p);
+      		}
+      	}
+      	return L;
+	}
+}
--- src/com/modes/UserTest.java
+++ src/com/modes/UserTest.java
@@ -1,5 +1,6 @@
 package com.modes;
 import java.util.Scanner;
+import java.util.List;
 
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
@@ -11,28 +12,27 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 import com.algorithms.Euclids;
 import com.algorithms.ConsecutiveIntegerCheck;
+import com.algorithms.Fibonacci;
+import com.algorithms.SieveOfEratosthenes;
+
 import com.chart.ScatterPlot;
 
 public class UserTest {
 	private static String MD_AVG_N = "MDavg(n)";
 	private static String D_AVG_N = "Davg(n)";
 	private static String TASK_1 = "Task 1";
+	private static String TASK_3 = "Task 3";
 
 	private static Euclids euclids = new Euclids();
 	private static ConsecutiveIntegerCheck cic = new ConsecutiveIntegerCheck();
+	private static Fibonacci fibonacci = new Fibonacci();
+	private static SieveOfEratosthenes eratosthenes = new SieveOfEratosthenes();
 
 	public static void run() {
 		runTask1();
-	}
+		runTask3();
+		// runTask2();
 
-	private static void generateScatterPlot(XYDataset dataset) {
-		SwingUtilities.invokeLater(() -> {
-	      ScatterPlot example = new ScatterPlot(TASK_1, dataset, MD_AVG_N, D_AVG_N);
-	      example.setSize(800, 400);
-	      example.setLocationRelativeTo(null);
-	      example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-	      example.setVisible(true);
-	    });
 	}
 
 	private static void runTask1() {
@@ -45,26 +45,48 @@ public class UserTest {
 		generateScatterPlot(dataset);
 	}
 
+	private static void runTask3() {
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		System.out.println("----- Running " + TASK_3 + " -----");
+		Integer m = getUserInput("m");
+		Integer n = getUserInput("n");
+
+		Integer gcd = eratosthenes.computeGCD(m, n);
+		System.out.println("GCD(m, n): " + gcd);	
+	}
+
 	private static XYSeries getAvgModDivisions(int n) {
 		double total = 0.0;
-		XYSeries modDivisions = new XYSeries(MD_AVG_N);
+		XYSeries series = new XYSeries(MD_AVG_N);
 		for (int i = 1; i < (n + 1); i++) {
-			total += euclids.getDivisionCountGCD(n, i);
-			modDivisions.add(i, total);
+			double divisions = euclids.getDivisionCountGCD(n, i);
+			total += divisions;
+			series.add(i, divisions);
 		}
 		System.out.println("Number of " + MD_AVG_N + " : " + total / n);
-		return modDivisions;
+		return series;
 	}
 
 	public static XYSeries getAvgDivisions(int n) {
 		double total = 0.0;
-		XYSeries divisions = new XYSeries(D_AVG_N);
+		XYSeries series = new XYSeries(D_AVG_N);
 		for (int i = 1; i < (n + 1); i++) {
-			total += cic.getDivisionCountGCD(n, i);
-			divisions.add(i, total);
+			double divisions = cic.getDivisionCountGCD(n, i);
+			total += divisions;
+			series.add(i, divisions);
 		}
 		System.out.println("Number of " + D_AVG_N  + " : " + total / n);
-		return divisions;
+		return series;
+	}
+
+	private static void generateScatterPlot(XYDataset dataset) {
+		SwingUtilities.invokeLater(() -> {
+	      ScatterPlot example = new ScatterPlot(TASK_1, dataset, MD_AVG_N, D_AVG_N);
+	      example.setSize(800, 400);
+	      example.setLocationRelativeTo(null);
+	      example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	      example.setVisible(true);
+	    });
 	}
 
 	private static Integer getUserInput(String var) {
@@ -82,4 +104,20 @@ public class UserTest {
 		}
 		return result;
 	}
+
+	// TODO
+	// private static void runTask2() {
+	// 	XYSeriesCollection dataset = new XYSeriesCollection();
+	// 	System.out.println("----- Running " + TASK_1 + " -----");
+	// 	Integer n = getUserInput("k");
+	// }
+
+	// private static XYSeries computeGDC(int k) {
+	// 	double totalModDivisions = 0.0;
+	// 	for (k; k >= 1; i++; i--) {
+	// 		int m = fibonacci.getNthElement(k);
+	// 		int n = fibonacci.getNthElement(k + 1);
+	// 	}
+	// }
+	///////
 }
