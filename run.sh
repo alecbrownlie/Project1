31a623ca9f972f143b653c22e1e0de6fb03ab4fd
Alec Brownlie <alecbrownlie@gmail.com>
2019-02-26 11:53:33 -0800

    adding run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# run Project 1 Application
+cd src
+export CLASSPATH=$CLASSPATH:lib/jcommon-1.0.23.jar:lib/jfreechart-1.0.19.jar 
+javac com/main/Application.java
+java com/main/Application
+cd ..
